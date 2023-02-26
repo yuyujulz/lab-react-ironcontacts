6c98cc9b9c8e4485b0ff97b67f1d3f1488911914
Julia Ganina <ganina.julia2@gmail.com>
2023-02-26 22:19:19 +0000

    iteration 2, 4 done

--- src/App.js
+++ src/App.js
@@ -5,16 +5,43 @@ import { useState } from 'react';
 
 function App() {
   const [contacts, setContacts] = useState(peopleContacts);
-
+  
+  // function addRandom(){
+  //   const randomContact = [...contacts]
+  //   randomContact[Math.floor(Math.random()*randomContact.length)]
+  // }
 
   //3 iteration 
-  const deleteContact = (contactId) => {
-    const filteredContact = contacts.filter((people) => {
-      return people._id !== contactId;
-    });
-    setContacts(filteredContact);
-  };
-
+  // const deleteContact = (contactId) => {
+  //   const filteredContact = contacts.filter((people) => {
+  //     return people._id !== contactId;
+  //   });
+  //   setContacts(filteredContact);
+  // };
+function sortContactsName() {
+  const sortedContacts = [...contacts]
+  sortedContacts.sort((a,b) => {
+   if(a.name<b.name){
+    return -1
+   }else if(b.name<a.name){
+    return 1
+   }
+   return 0
+  })
+  setContacts(sortedContacts)
+}
+function sortContactsRating() {
+  const sortedContacts = [...contacts]
+  sortedContacts.sort((a,b) => {
+   if(a.popularity<b.popularity){
+    return 1
+   }else if(b.popularity<a.popularity){
+    return -1
+   }
+   return 0
+  })
+  setContacts(sortedContacts)
+}
   
 
   
@@ -22,20 +49,34 @@ function App() {
     
     <div className="App">
       <h1>IronContacts</h1>
-      <table>
+      <button className='btn'>Add Random Contact</button>
+      <button className='btn' onClick={sortContactsName}>Sort by Name</button>
+      <button className='btn' onClick={sortContactsRating}>Sort by Popularity</button>
+      <table key={contacts._id}>
         <tr>
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
         </tr>
-        {contacts.map((people) => {
+        {contacts.slice(0, 5).map((people) => {
+          
           return(
             <tr>
 
-            <td><img src = {people.pictureUrl} className= "pic"/></td>
+            <td><img src ={people.pictureUrl} className="pic" alt='celebrity'/></td>
             <td>{people.name}</td>
             <td>{people.popularity}</td>
-            <td></td>
+ {/* iteration 2 */}
+            {people.wonOscar && <td>🏆</td>}
+            {!people.wonOscar && <td></td>}
+           
+            {people.wonEmmy && <td>🏆</td>}
+            {!people.wonEmmy && <td></td>}
+           
+            
+            <td><button className='btn'>Delete</button></td>
             </tr>
 
 
@@ -44,6 +85,6 @@ function App() {
       </table>
     </div>
   );
-}
+      }
 
 export default App;
